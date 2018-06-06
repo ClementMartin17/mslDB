1400777faec416e0eb3665e5ac2c15ad82706d71
Clement <clementmartin1708@gmail.com>
2018-06-06 16:25:43 +0200

    stat component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,21 +2,24 @@ import { NgModule } from "@angular/core";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { AppRoutingModule } from "../app-routing.module";
+import { StatisticComponent } from "./statistic/statistic.component";
 
 
 @NgModule({
-  declarations: [ 
+  declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    StatisticComponent
   ],
   exports:[
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    StatisticComponent
   ],
   imports: [
     AppRoutingModule
     ],
   providers: [],
- 
+
 })
 export class LayoutModule { }
---
+++ src/app/layout/statistic/statistic.component.ts
@@ -0,0 +1,82 @@
+import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
+import * as d3 from 'd3';
+@Component({
+  selector: 'msl-stat',
+  templateUrl: './statistic.component.html',
+  styleUrls: ['./statistic.component.css']
+})
+export class StatisticComponent implements OnInit,AfterViewInit {
+  @Input() stat: number;
+  @Input() type: string;
+  id: string;
+  ngOnInit() {
+    this.id = `${this.type}${this.stat}`;
+  }
+
+  ngAfterViewInit() {
+    let maxValue: number;
+    switch (this.type) {
+      case 'hp':
+        maxValue = 49696;
+        break;
+        case 'attack':
+        maxValue = 3807;
+        break;
+        case 'defense':
+        maxValue = 3364;
+        break;
+        case 'recovery':
+        maxValue = 49696;
+        break;
+        case 'crit':
+        maxValue = 100;
+        break;
+        case 'critdmg':
+        maxValue = 100;
+        break;
+        case 'resist':
+        maxValue = 85;
+        break;
+    }
+
+    const width = 280;
+    const height = 20;
+
+    this.stat = (this.stat * width) / maxValue;
+
+    const x = d3
+      .scaleLinear()
+      .domain([0, 10000])
+      .range([0, width]);
+
+    const svg = d3
+      .select(`#${this.id}`)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g');
+
+    svg
+      .append('rect')
+      .attr('class', 'grid-background')
+      .attr('width', width)
+      .attr('height', height);
+
+    svg
+      .append('rect')
+      .attr('class', `grid-fill-${this.type}`)
+      .attr('width', this.stat)
+      .attr('height', height);
+
+    svg
+      .append('g')
+      .attr('class', 'grid')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(
+        d3
+          .axisBottom(x)
+          .ticks(6)
+          .tickSize(-20)
+      );
+  }
+}
